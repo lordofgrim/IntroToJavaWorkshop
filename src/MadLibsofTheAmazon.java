f7a4f7882aa225faeb86d5afe70b50815ff9ac86
lordofgrim <league@Mac-mini.local>
2016-06-09 15:01:25 -0700

    good
    learned to be a creeper

---
+++ src/MadLibsofTheAmazon.java
@@ -0,0 +1,34 @@
+
+import javax.swing.JOptionPane;
+
+public class MadLibsofTheAmazon {
+	public static void main(String[] args) {
+
+		// Put this sentence in a pop up:
+		// If you find yourself having to cross a piranha-infested river, here's how to do it...
+JOptionPane.showMessageDialog(null, "if you find yourself having to cross a piranha-infested river,here's how to do it");
+		// Get the user to enter an adjective
+String adjectives=JOptionPane.showInputDialog("can you pleas give me a adjective");
+		// Get the user to enter a type of liquid
+String liquid=JOptionPane.showInputDialog("can you give me a type of liquid");
+		// Get the user to enter a body part
+String body=JOptionPane.showInputDialog("can you give me a body part");
+		// Get the user to enter a verb
+String verb=JOptionPane.showInputDialog("can you give me a verb");
+		// Get the user to enter a place
+String place=JOptionPane.showInputDialog("can you give me a place");
+		// Fit the user's words into this sentence, and save it in a String:
+		// Piranhas are more [adjective] during the day, so cross the river at
+		// night. Piranhas are attracted to fresh [type of liquid] and will most
+		// likely take a bite out of your [body part] if you [verb]. Whatever
+		// you do, if you have an open wound, try to find another way to get
+		// back to the [place]. Good luck!
+		JOptionPane.showMessageDialog(null, "piranhas are more " +adjectives+ " during the day so cross the river at night.piranhas are attracted to fresh "+liquid+" and will most likely take a bite"
+				+ "out of your "+body+" if you "+verb+" .whatever you "
+						+ "do,if you have a open wound, try to find another way to get back to the "+place+".good luck");
+		// Make a pop-up for the final story. You can use \n to go to the next line.
+		
+
+	}
+}
+
